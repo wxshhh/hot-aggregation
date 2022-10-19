a51a33c039a3190f87a339a5ce9b9c90844cea42
A-soul-Ava <91597059+A-soul-Ava@users.noreply.github.com>
2022-10-20 00:34:29 +0800

    修改前端代码
    修改前端代码

---
+++ frontend/src/components/RankComponent.vue
@@ -0,0 +1,25 @@
+<template>
+    <div v-if="rank==1" style="padding-top:10px;padding-right:5px;">
+        <img src="https://www.suredian.com/static/images/index/top1.svg" width="50" alt="">
+    </div>
+    <div v-else-if="rank==2" style="padding-top:10px;padding-right:5px;">
+        <img src="https://www.suredian.com/static/images/index/top2.svg" width="50" alt="">
+    </div>
+    <div v-else-if="rank==3" style="padding-top:10px;padding-right:5px;">
+        <img src="https://www.suredian.com/static/images/index/top3.svg" width="50" alt="">
+    </div>
+    <div v-else>
+        <h1 style="text-align:center;">
+            NO.{{rank}}
+        </h1>
+    </div>
+</template>
+
+<script>
+    export default {
+    props: ['rank'],
+    setup() {
+
+    }
+}
+</script>
